e164bc2767cc107240b520789fcd97e06e2697e0
Bodernet <142262665+Bodernet@users.noreply.github.com>
2023-11-17 22:57:00 +0200

    Task 1-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,19 @@
-function filterArray(numbers, value) {
-  let newNumb = [];
-  for (const number of numbers) {
-    if (number > value) {
-      newNumb.push(number);
-    }
-  }
-  return newNumb;
-}
+// function filterArray(numbers, value) {
+//   let newNumb = [];
+//   for (const number of numbers) {
+//     if (number > value) {
+//       newNumb.push(number);
+//     }
+//   }
+//   return newNumb;
+// }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
